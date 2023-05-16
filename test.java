94b0a0c4ea68193910a6125b6516befe5beb93b2
newFantasy <junyouna0713@gmail.com>
2023-05-16 19:04:55 +0900

    change princess to youna

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("I am 공주");
-        System.out.println("I am 공주");
+        System.out.println("I am Youna");
+        System.out.println("I am Youna");
     }
 }
